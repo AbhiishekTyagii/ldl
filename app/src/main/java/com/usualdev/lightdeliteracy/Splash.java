1f02b32cbf76d1c97d1549a2e919130b6323924c
Abhishek Tyagi <developers.usualdev@gmail.com>
2024-09-11 19:09:50 +0530

    Added Splash Screens

---
+++ app/src/main/java/com/usualdev/lightdeliteracy/Splash.java
@@ -0,0 +1,46 @@
+package com.usualdev.lightdeliteracy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Splash extends AppCompatActivity {
+
+
+
+        FirebaseUser currentUser;
+        private FirebaseAuth mAuth;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_splash);
+            mAuth = FirebaseAuth.getInstance();
+            currentUser = mAuth.getCurrentUser();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    if (user == null) {
+                        Intent intent = new Intent(Splash.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Intent mainIntent = new Intent(Splash.this,LoginActivity.class);
+                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(mainIntent);
+                        finish();
+                    }
+                }
+            }, 2000);
+        }
+    }
